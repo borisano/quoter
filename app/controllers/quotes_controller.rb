d73c7e812da3dd4524045b92ec7e7af44ed66008
Anatolii Borysov <borisano@gmail.com>
2023-08-15 15:52:17 +0100

    Only show Quotes from current Company

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,7 @@ class QuotesController < ApplicationController
   before_action :set_quote, only: [:show, :edit, :update, :destroy]
 
   def index
-    @quotes = Quote.all.ordered
+    @quotes = current_company.quotes.ordered.ordered
   end
 
   def show
